250c1512f940a41688992077dcd0e2aa1106f15c
gary091239 <gary091239@gmail.com>
2018-05-30 00:01:23 +0800

    test

--- app.py
+++ app.py
@@ -42,5 +42,7 @@ def handle_message(event):
         TextSendMessage(text=content))
 
 
+import os
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
